744e24c701a9b10d89c2bb791a9172c733fba045
joeyverbeke <joeyverbeke@gmail.com>
2023-11-12 19:53:24 -0800

    change to timeout, add phrases

--- script.js
+++ script.js
@@ -6,6 +6,30 @@ let webcamRunning = false;
 const videoWidth = 480;
 let blinking = false;
 
+let phrases = 
+[
+    "identity’s silent // seamless stream",
+    "unconscious // conscious blooms",
+    "continuity // identity's dance",
+    "veil lifted // self revealed",
+    "unseen journey // conscious arrival",
+    "unconscious threads // conscious loom",
+    "eternal self // veiled voyage",
+    "silent dialogue // spoken being",
+    "unveiling // identity's choreography",
+    "flow of unseen // seen self",
+    "continuous whisper // spoken being",
+    "dance // veiled revelations",
+    "identity // a silent river’s flow",
+    "unconscious echo // conscious light",
+    "unveiling // endless self",
+    "veiled voyage // known shores",
+    "unending narrative // self unveiled",
+    "silent tapestry // vivid becoming",
+    "conscious veil // unconscious reveal",
+    "unbroken thread // woven being"
+];
+
 async function createFaceLandmarker() {
     const filesetResolver = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm");
     faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
@@ -57,25 +81,50 @@ async function predictWebcam() {
         results = await faceLandmarker.detectForVideo(video, startTimeMs);
     }
     if (results) {
-        drawBlendShapes(results.faceBlendshapes);
+        detectBlinking(results.faceBlendshapes);
     }
     if (webcamRunning) {
         window.requestAnimationFrame(predictWebcam);
     }
 }
 
-function drawBlendShapes(blendShapes) {
+function detectBlinking(blendShapes) {
     if (!blendShapes.length) {
         return;
     }
-    let currentlyBlinking = blendShapes[0].categories[9].score > 0.7 || 
-                            blendShapes[0].categories[10].score > 0.7;
+    let currentlyBlinking = blendShapes[0].categories[9].score > 0.5 || 
+                            blendShapes[0].categories[10].score > 0.5;
 
     if (currentlyBlinking && !blinking) {
         blinking = true;
-        document.body.style.backgroundColor = "black";
+        triggerBlinkAction();
     } else if (!currentlyBlinking && blinking) {
         blinking = false;
-        document.body.style.backgroundColor = "white";
     }
+}
+
+function triggerBlinkAction() {
+    // Change background to black
+    document.body.style.backgroundColor = "black";
+
+    // Choose a random message
+    const phrase = phrases[Math.floor(Math.random() * phrases.length)];
+
+    // Create a text element and add it to the screen
+    const textElement = document.createElement('div');
+    textElement.style.position = 'absolute';
+    textElement.style.top = '50%';
+    textElement.style.left = '50%';
+    textElement.style.transform = 'translate(-50%, -50%)';
+    textElement.style.color = 'white';
+    textElement.style.fontSize = '24px';
+    textElement.style.zIndex = '1000';
+    textElement.innerText = phrase;
+    document.body.appendChild(textElement);
+
+    // Set a timeout to revert the action and remove the text after 100ms
+    setTimeout(() => {
+        document.body.style.backgroundColor = "white"; // Revert background color
+        document.body.removeChild(textElement); // Remove the text element
+    }, 25);
 }
