22fddad3ea15d26323a642ba7d7972ca1ecd8124
joeyverbeke <joeyverbeke@gmail.com>
2023-11-18 22:04:02 -0800

    create branch

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 import vision from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
 
+//for branch
+
 const { FaceLandmarker, FilesetResolver } = vision;
 let faceLandmarker;
 let runningMode = "IMAGE";
